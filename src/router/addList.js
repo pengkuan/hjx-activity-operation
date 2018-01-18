7b0cf56825ec0dc986e77db6d7162bfd3345c03d
tianyu <tianyu@huishoubao.com.cn>
2018-01-18 11:36:18 +0800

    广告组件目录划分

--- src/router/addList.js
+++ src/router/addList.js
@@ -1,9 +1,15 @@
 const hjxCustomer = resolve => require(['@/views/adList/hjxCustomer'], resolve) 
 const hjxD = resolve => require(['@/views/adList/hjxD'], resolve) 
-const hjxS = resolve => require(['@/views/adList/hjxS'], resolve)      
+const hjxS = resolve => require(['@/views/adList/hjxS'], resolve) 
+const commonDetail = resolve => require(['@/views/adList/commonDetail'], resolve)  
+const commonAdd = resolve => require(['@/views/adList/commonAdd'], resolve)  
+const commonEdit = resolve => require(['@/views/adList/commonEdit'], resolve)     
 
 export default {
 	hjxCustomer,
 	hjxD,
-	hjxS
+	hjxS,
+	commonDetail,
+	commonAdd,
+	commonEdit
 }
