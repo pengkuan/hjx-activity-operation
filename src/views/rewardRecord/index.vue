13d0b49c4f0d91b017ef28a42e88cabb09ed7bd0
tianyu <tianyu@huishoubao.com.cn>
2018-03-27 20:15:10 +0800

    付款金额不要小数

--- src/views/rewardRecord/index.vue
+++ src/views/rewardRecord/index.vue
@@ -75,7 +75,7 @@
             <el-table-column prop="orderNum" label="订单编号"></el-table-column>
             <el-table-column label="付款金额">
                 <template slot-scope="scope">
-                    <span> {{(scope.row.orderPrice / 100).toFixed(2)}} </span>
+                    <span> {{scope.row.orderPrice / 100}} </span>
                 </template>
             </el-table-column>
             <el-table-column width="100" label="奖励类型">
